78936d30e4c13834dad74ecc33d7beab3887d1e7
unknown <behrooz.arabzade@gmail.com>
2020-07-19 19:16:27 +0430

    before change whole structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React from 'react';
 
-export default class App extends React.Component{
-  render () {
-    return (
-      <iframe src={`http://localhost:3000/`} style={{
-        width: "80vw",
-        height: "80vh"
-      }}>
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-      </iframe>
-    )
-  }
+        this.rootRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // TODO test, clean them after test finished
+        window.addEventListener("keydown",(e) =>{
+            e = e || window.event;
+            let key = e.which || e.keyCode; // keyCode detection
+            let ctrl = e.ctrlKey ? e.ctrlKey : (key === 17); // ctrl detection
+
+            if ( key === 88 && ctrl ) {
+                e.preventDefault();
+                console.log("ctrl + X");
+                let start = new Date().getTime();
+                let rect = this.rootRef.current.getBoundingClientRect();
+                let end = new Date().getTime();
+                console.log("rect", rect, end - start);
+
+            }
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <iframe src={`http://localhost:3000/`} style={{
+                    width: "80vw",
+                    height: "80vh"
+                }}>
+
+                </iframe>
+                <div ref={this.rootRef}>
+                    Behrooz
+                </div>
+            </>
+        )
+    }
 }
